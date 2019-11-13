7f6bcdcb28aa84880ee5fadfb5d95a4883c90fd3
Michael Delgado <delgado.portland@gmail.com>
2019-11-12 21:58:36 -0800

    Update BasicReport model fields with peer review fields

--- reports/report_types/models.py
+++ reports/report_types/models.py
@@ -1,35 +1,150 @@
+import datetime
+import decimal
+import uuid
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 
 def get_sentinel_user():
     return get_user_model().objects.get_or_create(username='deleted')[0]
 
+def current_year():
+        return int(datetime.datetime.now().year)
+
+def number():
+    if BasicReport.objects.exists():
+        last_year_entered = (
+            BasicReport.objects.last().report_id_year
+            )
+        if current_year == last_year_entered:
+            no = BasicReport.objects.filter(
+                report_id_year=current_year
+                ).select_for_update().aggregate(
+                Max('report_id_number')
+                )
+            no = int(no['report_id_number__max'])+1
+            no_string = str(no).zfill(9)
+            return '{}'.format(no_string)
+        else:
+            return '1'.zfill(9)
+    else:
+        return '1'.zfill(9)
+
+
 class BasicReport(models.Model):
-	"""docstring for BasicReport"""
-	author = models.ForeignKey(
-		settings.AUTH_USER_MODEL,
-		on_delete=models.SET(get_sentinel_user),
-		related_name="author",
-		)
-	title = models.CharField(max_length=500)
-	executive_summary = models.TextField(blank=True) 
-	introduction = models.TextField(blank=True)
-	body = models.TextField(blank=True) 
-	conclusion = models.TextField(blank=True) 
-	recommendations = models.TextField(blank=True)
-	references = models.TextField(blank=True) 
-	reviewer = models.ForeignKey(
-		settings.AUTH_USER_MODEL,
-		on_delete=models.SET(get_sentinel_user),
-		related_name="reviewer",
-		)
+    """docstring for BasicReport"""
+    report_id_number = models.CharField(
+        default=number, 
+        editable=False, 
+        max_length=10
+        )
+    report_id_year = models.PositiveIntegerField(
+        default=current_year, 
+        editable=False
+        )
+    version = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2, 
+        editable=False,
+        )
+    correspondence_version = models.DecimalField(
+        max_digits=5, 
+        decimal_places=2, 
+        editable=False,
+        )
+    unique_id = models.UUIDField(
+        default=uuid.uuid4, 
+        editable=False, 
+        unique=True
+        )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_sentinel_user),
+        related_name="author",
+        )
+    author_as_string = models.CharField(
+        max_length=300, 
+        blank=True,
+        )
+    title = models.CharField(
+        max_length=1000, 
+        unique=True, 
+        blank=True, 
+        null=True
+        )
+    title_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    executive_summary = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    executive_summary_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        ) 
+    introduction = models.TextField(
+        max_length=100000, 
+        blank=True,
+         )
+    introduction_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    body = models.TextField(
+        max_length=100000, 
+        blank=True,
+        ) 
+    body_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    conclusion = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    conclusion_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        ) 
+    recommendations = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    recommendations_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    references = models.TextField(
+        max_length=100000, 
+        blank=True,
+        ) 
+    references_peer_review = models.TextField(
+        max_length=100000, 
+        blank=True,
+        )
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET(get_sentinel_user),
+        related_name="reviewer",
+        )
+    reviewer_as_string = models.CharField(
+        max_length=300, 
+        blank=True, 
+        )
+    edited_in_admin_by = models.CharField(
+        max_length=300, 
+        blank=True,
+        )
 
 
-	def __init__(self, arg):
-		super(BasicReport, self).__init__()
-		self.arg = arg
+    def __init__(self, arg):
+        super(BasicReport, self).__init__()
+        self.arg = arg
 
 
-		
+        
