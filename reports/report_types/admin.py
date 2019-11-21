ec565dcbeb6cd52abc90c243928514076c837d14
Michael Delgado <delgado.portland@gmail.com>
2019-11-21 14:20:47 -0800

    Add BasicReport and BasicReportVersion admin

--- reports/report_types/admin.py
+++ reports/report_types/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from report_types.models import BasicReport, BasicReportVersion
+
+
+class BasicReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title',)
+
+class BasicReportVersionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title',)
+
+
+admin.site.register(BasicReport, BasicReportAdmin)
+admin.site.register(BasicReportVersion, BasicReportVersionAdmin)
