c5cc3b319550dbba9c9698045e073b468d75b7d7
Michael Delgado <delgado.portland@gmail.com>
2019-11-21 14:05:06 -0800

    Add Status admin

--- reports/status/admin.py
+++ reports/status/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from status.models import Status
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title',)
+    list_display_links = ('id', 'title',)
+
+
+admin.site.register(Status, StatusAdmin)
