112a36d7353e9bf4a26926434133f8ec3332ce3c
Michael Delgado <delgado.portland@gmail.com>
2019-11-22 10:58:48 -0800

    Add tags admin

--- reports/tags/admin.py
+++ reports/tags/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from tags.models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'date_created', 'date_modified',
+    	'created_by', 'modified_by')
+    list_display_links = ('id', 'title', 'date_created', 'date_modified',
+    	'created_by', 'modified_by')
+    order = ('id',)
+    list_filter = ['id', 'title', 'date_created', 'date_modified',
+    	'created_by', 'modified_by']
+    search_fields = ['title']
+    list_per_page = 20 
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'created_by', None) is None:
+            obj.created_by = request.user.username
+        else:
+        	obj.modified_by = request.user.username
+        obj.save()
+
+
+admin.site.register(Tag, TagAdmin)
