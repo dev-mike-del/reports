c073f2e0849580745cade5f1d2037d18a174208c
Michael Delgado <delgado.portland@gmail.com>
2019-11-22 12:51:12 -0800

    Add BasicReportFormView

--- reports/report_types/views.py
+++ reports/report_types/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import FormView
 
-from status.models import Status
+from report_types.forms import BasicReportForm
 
+
+class BasicReportFormView(object):
+	"""docstring for BasicReportForm"""
+	template_name = 'basic_report_form.html'
+	form_class = BasicReportForm
+	success_url = ''
