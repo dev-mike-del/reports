f281881e302197094f6dfbd5195cef9134c45994
Michael Delgado <delgado.portland@gmail.com>
2019-11-22 12:10:53 -0800

    Add report_admin/ to main url conf

--- reports/reports/urls.py
+++ reports/reports/urls.py
@@ -18,4 +18,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('report_admin/',
+        include(
+            'report_types.urls', 
+            namespace='reports',
+            )
+        ),
 ]
