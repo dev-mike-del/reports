19947ab489e5125627deebd8524e0a5a8495ff52
Michael Delgado <delgado.portland@gmail.com>
2019-12-04 18:39:36 -0800

    Update basic report form with peer review fields

--- reports/report_admin/forms.py
+++ reports/report_admin/forms.py
@@ -7,11 +7,19 @@ class BasicReportForm(ModelForm):
         model = BasicReport
         fields = [
         	'title',
+            'title_peer_review',
         	'executive_summary',
+            'executive_summary_peer_review',
         	'introduction',
+            'introduction_peer_review',
         	'body',
+            'body_peer_review',
         	'conclusion',
+            'conclusion_peer_review',
         	'recommendations',
+            'recommendations_peer_review',
         	'references',
+            'references_peer_review',
         	'tags_as_string',
+            'tags_peer_review',
         	'reviewer',]
