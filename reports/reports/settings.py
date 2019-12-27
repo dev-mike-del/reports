536556f6179749381a063f9f554386fa62b06712
Michael Delgado <delgado.portland@gmail.com>
2019-12-27 10:01:40 -0800

    A .ebextensions and update allowed_hosts

--- reports/reports/settings.py
+++ reports/reports/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reports-env.55kcu7mxm7.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
